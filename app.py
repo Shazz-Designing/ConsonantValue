432352985f25b51c0c509fb000d41eaad3e6bde7
sharon <sapot@safaricom.co.ke>
2023-12-18 08:40:09 +0300

    Run tests

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 def solve(s):
-    def calculate_value(string):
-        return sum(ord(char) - ord('a') + 1 for char in string)
+    def calculate_value(substring):
+        return sum(ord(char) - ord('a') + 1 for char in substring)
 
     consonant_substrings = [substr for substr in s.split('a') if substr]
     values = [calculate_value(substr) for substr in consonant_substrings]
 
     return max(values, default=0)
-
-# Example
-result = solve("zodiacs")
-print(f"The highest value of consonant substrings is: {result}")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,10 @@
-from app import highest_consonant_value
+import sys
+import os
 
-def test_highest_consonant_value():
-    assert highest_consonant_value("zodiacs") == 26
-    assert highest_consonant_value("strength") == 57
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import solve
+
+def test_solve():
+    assert solve("zodiacs") == 54
+    assert solve("strength") == 111
